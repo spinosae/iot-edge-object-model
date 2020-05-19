185cda673a9a7036b2d5b4bf02c96251a0960904
spinosae <yanghua.que@gmail.com>
2020-05-20 00:03:36 +0800

    export patch models and methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { generateConfigurationContent } from './generators/edgeConfigurationContentGenerator';
+export { generateConfigurationContent } from './generators/edgeConfigurationContentPatchGenerator';
 export { toEdgeConfigurationContentViewModel } from './parsers/edgeConfigurationContentParser';
+export { toEdgeConfigurationContentPatchViewModel } from './parsers/edgeConfigurationContentPatchParser';
 export { to$EdgeAgentModuleTwinViewModel } from './parsers/$EdgeAgentModuleTwinParser';
 export { to$EdgeHubModuleTwinViewModel } from './parsers/$EdgeHubModuleTwinParser';
 export { convertToEdgeConfigurationContentViewModel, convertToEdgeModuleViewModels } from './utilities/viewModelTransforms';
